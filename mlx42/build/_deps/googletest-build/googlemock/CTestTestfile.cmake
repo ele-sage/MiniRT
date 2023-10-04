9f0de1c97289f8a3c960f6f876feec0be41075b4
Émile Le Sage <83116125+ele-sage@users.noreply.github.com>
2023-10-03 21:53:50 -0400

    Delete mlx42 directory

--- mlx42/build/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ele-sage/Desktop/42/Core/MiniRT/MLX42/build/_deps/googletest-src/googlemock
-# Build directory: /Users/ele-sage/Desktop/42/Core/MiniRT/MLX42/build/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
