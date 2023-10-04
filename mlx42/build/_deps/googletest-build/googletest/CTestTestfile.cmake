929a565e887aa609ca0799308dcb38854d1f2e11
Emile Le Sage <emile.lesage8@gmail.com>
2023-10-03 21:50:57 -0400

    MiniRT

---
+++ mlx42/build/_deps/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/ele-sage/Desktop/42/Core/MiniRT/MLX42/build/_deps/googletest-src/googletest
+# Build directory: /Users/ele-sage/Desktop/42/Core/MiniRT/MLX42/build/_deps/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- srcs/main.c
+++ srcs/main.c
@@ -6,17 +6,42 @@
 /*   By: ele-sage <ele-sage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 20:40:31 by ele-sage          #+#    #+#             */
-/*   Updated: 2023/10/03 21:09:43 by ele-sage         ###   ########.fr       */
+/*   Updated: 2023/10/03 21:49:25 by ele-sage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-int main(int argc, char **argv)
-{
-    (void)argc;
-    (void)argv;
+#define WIDTH 256
+#define HEIGHT 256
 
-    printf("Hello World!\n");
-    return (0);
+// Exit the program as failure.
+static void ft_error(void)
+{
+	fprintf(stderr, "%s", mlx_strerror(mlx_errno));
+	exit(EXIT_FAILURE);
+}
+
+// Write MiniRT on the window.
+static void write_on_window(mlx_t* mlx)
+{
+    mlx_put_string(mlx, "MiniRT", 10, 10);
+}
+
+int32_t	main(void)
+{
+
+	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "MiniRT", true);
+	if (!mlx)
+		ft_error();
+
+	mlx_image_t* img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
+		ft_error();
+
+    write_on_window(mlx);
+
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
 }
